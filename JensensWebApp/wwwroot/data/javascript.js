69e626cce84b625f910aa59be0cf47c1d7909621
MelonSylt <elia.at@hotmail.com>
2023-06-15 19:22:49 +0200

    Fixa till dark mode, Så det funkar på hela headern inc logo, body, ändra position på dark-mode knappen och förfina den.

--- JensensWebApp/wwwroot/data/javascript.js
+++ JensensWebApp/wwwroot/data/javascript.js
@@ -1,8 +1,14 @@
 
 
-document.getElementById('darkModeBtn').addEventListener('click', function () {
+    document.getElementById('darkModeBtn').addEventListener('click', function () {
     document.body.classList.toggle('dark-mode');
     document.getElementById('navbar').classList.toggle('dark-mode');
+
+    var navLinks = document.getElementsByClassName('nav-link');
+
+    for (var i = 0; i < navLinks.length; i++) {
+        navLinks[i].classList.toggle('nav-link-dark');
+    }
 });
 
 function displayCurrentDate() {
